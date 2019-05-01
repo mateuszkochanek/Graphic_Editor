20dc89bfe0077eb8850d946557995e2c7e520b64
Erthax <mateuszkochanek01@gmail.com>
2019-05-01 20:48:29 +0200

    #02.Chec Box disables Shape Creation Buttons

--- Edytor/src/main/java/Edytor/App.java
+++ Edytor/src/main/java/Edytor/App.java
@@ -12,6 +12,8 @@ import java.awt.event.ComponentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -23,9 +25,6 @@ import javax.swing.JToolBar;
 
 class Surface extends JPanel {
     public Graphics2D g2d;
-    private int locX = 0;
-    private int locY = 0;
-    private String FigName;
     public List<Figure> figures = new ArrayList<>();
 
     public Surface() {
@@ -46,11 +45,23 @@ class Figure {
     private int locX = 0;
     private int locY = 0;
     private String FigName;
+    private Rectangle2D rect;
+    private Ellipse2D ellipse;
 
     public Figure(int locX, int locY, String x) {
         this.locX = locX;
         this.locY = locY;
         FigName = x;
+        if (FigName.compareTo("Circle") == 0)
+        {
+            ellipse = new Ellipse2D.Float(locX, locY, 100f, 100f);
+        }
+        if (FigName.compareTo("Rectangle") == 0)
+        {
+            rect = new Rectangle2D.Float(locX, locY, 200f, 100f);
+        }
+        if (FigName.compareTo("Polygon") == 0)
+            ;
     }
 
     public void draw(Graphics g) {
@@ -58,16 +69,16 @@ class Figure {
         g2d.setColor(Color.RED);
 
         if (FigName.compareTo("Circle") == 0)
-            g2d.fillOval(locX, locY, 100, 100);
+            g2d.fill(ellipse);
         if (FigName.compareTo("Rectangle") == 0)
-            g2d.fillRect(locX, locY, 200, 100);
+            g2d.fill(rect);
         if (FigName.compareTo("Polygon") == 0)
             ;
         // g2d.fillOval(locX, locY, 10, 10);
     }
 }
 
-class ClickAdapter extends MouseAdapter /* implements Runnable */ {
+class ClickAdapter extends MouseAdapter /* Adapter used for creating new shapes */ {
     public int x;
     public int y;
     public String Name;
@@ -87,15 +98,6 @@ class ClickAdapter extends MouseAdapter /* implements Runnable */ {
         surface.repaint();
         System.out.println(Name);
     }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public Figure GetFigure() {
-        return figure;
-    }
-
 }
 
 class MyButton extends JButton implements ActionListener {
@@ -117,7 +119,38 @@ class MyButton extends JButton implements ActionListener {
         surface.addMouseListener(new ClickAdapter(super.getText(), surface));
     }
 }
+class MyCheckBox extends JCheckBox implements ActionListener{
+    Surface surface;
+    MyButton CircleButton,RectangleButton,PolygonButton;
+    public MyCheckBox(String text, Surface sur, MyButton C, MyButton R, MyButton P) {
+        surface = sur;
+        super.setText(text);
+        addActionListener(this);
+        CircleButton = C; 
+        RectangleButton = R; 
+        PolygonButton = P;
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        MouseListener[] listerners = surface.getMouseListeners();
+        for (MouseListener listener : listerners) {
+            surface.removeMouseListener(listener);
+        }
+        if(super.isSelected())
+        {
+            CircleButton.setEnabled(false);
+            RectangleButton.setEnabled(false);
+            PolygonButton.setEnabled(false);
+        }
+        else
+        {
+            CircleButton.setEnabled(true);
+            RectangleButton.setEnabled(true);
+            PolygonButton.setEnabled(true);
+        }
+    }
+}
 
 public class App extends JFrame {
     private Surface surface;
@@ -139,15 +172,15 @@ public class App extends JFrame {
     private void CreateToolBar() {
 
         JToolBar toolbar = new JToolBar("Options",1);
-        MyButton CButton = new MyButton("Circle", surface);
-        MyButton RButton = new MyButton("Rectangle", surface);
-        MyButton PButton = new MyButton("Polygon", surface);
-        JCheckBox Edit  = new JCheckBox("Edit");
+        MyButton CircleButton = new MyButton("Circle", surface);
+        MyButton RectangleButton = new MyButton("Rectangle", surface);
+        MyButton PolygonButton = new MyButton("Polygon", surface);
+        MyCheckBox EditButton  = new MyCheckBox("Edit",surface,CircleButton,RectangleButton,PolygonButton);
 
-        toolbar.add(CButton);
-        toolbar.add(RButton);
-        toolbar.add(PButton);
-        toolbar.add(Edit);
+        toolbar.add(CircleButton);
+        toolbar.add(RectangleButton);
+        toolbar.add(PolygonButton);
+        toolbar.add(EditButton);
 
         add(toolbar, BorderLayout.EAST);
     }
